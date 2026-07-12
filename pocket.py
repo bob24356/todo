2ddc269a916ba39f5afa3ad03e43cd0f1226a40a
bob24356 <15511906317@163.com>
2026-07-12 15:34:58 +0800

    v1.5.0

--- pocket.py
+++ pocket.py
@@ -1,8 +1,8 @@
 from PySide6.QtWidgets import (QLabel, QTreeWidget,
-                               QGridLayout, QWidget, QPushButton,
+                               QGridLayout, QPushButton,
                                QLineEdit, QMessageBox, QTreeWidgetItem,
                                QMenu, QDialog, QRadioButton, QFileDialog,
-                               QSlider)
+                               QSlider, QMainWindow, QWidget, QFrame)
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QColor, QAction
 from pygame.mixer import init
@@ -12,7 +12,7 @@ from os import makedirs, path
 #import model
 
 
-class ToDo(QWidget):
+class ToDo(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -22,87 +22,28 @@ class ToDo(QWidget):
             'mid': '#ffffcc',
             'low': '#ccffcc'
         }
-        self.top_value = True
 
         self.setWindowTitle('待办')
-        self.setGeometry(300, 300, 600, 330)
+        self.setGeometry(300, 300, 600, 371)
         self.mainGrid = QGridLayout()
         self.tGrid = QGridLayout()
         self.bGrid = QGridLayout()
-        self.mGrid = QGridLayout()
-        self.setLayout(self.mainGrid)
-        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
 
         self.listWindow = QTreeWidget()
         self.listWindow.setColumnCount(2)
         self.listWindow.setHeaderLabels(['名称', '优先级'])
-        self.listWindow.setColumnWidth(0, 250)
+        self.listWindow.setColumnWidth(0, 350)
         self.listWindow.setColumnWidth(1, 80)
         self.listWindow.setSelectionMode(QTreeWidget.MultiSelection)
         self.listWindow.setRootIsDecorated(False)
 
         self.label = QLabel('待办事项')
         self.label.setAlignment(Qt.AlignCenter)
-        self.status_label = QLabel('状态：中等级')
 
         self.addPButton = QPushButton('添加')
-        self.addPButton.setStyleSheet("""
-            background-color: #0f4f94;
-            color: white;
-            padding: 10px;
-            font-weight: bold;
-        """)
         self.addPButton.clicked.connect(self.add_item)
         self.deletePButton = QPushButton('删除')
-        self.deletePButton.setStyleSheet("""
-            background-color: #0f4f94;
-            color: white;
-            padding: 10px;
-            font-weight: bold;
-        """)
         self.deletePButton.clicked.connect(self.remove_item)
-        self.highPButton = QPushButton('高等级')
-        self.highPButton.setStyleSheet("""
-            background-color: #FF5722;
-            color: white;
-            padding: 10px;
-            font-weight: bold;
-        """)
-        self.highPButton.clicked.connect(self.high)
-        self.midPButton = QPushButton('中等级')
-        self.midPButton.setStyleSheet("""
-            background-color: #e8d141;
-            color: white;
-            padding: 10px;
-            font-weight: bold;
-        """)
-        self.midPButton.clicked.connect(self.mid)
-        self.lowPButton = QPushButton('低等级')
-        self.lowPButton.setStyleSheet("""
-            background-color: #41c671;
-            color: white;
-            padding: 10px;
-            font-weight: bold;
-        """)
-        self.lowPButton.clicked.connect(self.low)
-        self.savePButton = QPushButton('保存')
-        self.savePButton.setStyleSheet("""
-                    background-color: #fdd35e;
-                    color: white;
-                    padding: 10px;
-                    font-weight: bold;
-                """)
-        self.savePButton.clicked.connect(self.save_everywhere)
-        self.openPButton = QPushButton('导入')
-        self.openPButton.setStyleSheet("""
-                            background-color: #fdd35e;
-                            color: white;
-                            padding: 10px;
-                            font-weight: bold;
-                        """)
-        self.openPButton.clicked.connect(self.open_everywhere)
-        self.help = QPushButton('帮助')
-        self.help.clicked.connect(self.show_help)
 
         self.startmPButton = QPushButton('开始')
         self.startmPButton.clicked.connect(self.startmusic)
@@ -110,8 +51,6 @@ class ToDo(QWidget):
         self.pausemPButton.clicked.connect(self.pausemusic)
         self.stopmPButton = QPushButton('停止')
         self.stopmPButton.clicked.connect(self.stopmusic)
-        self.openmPButton = QPushButton('导入')
-        self.openmPButton.clicked.connect(self.openmusic)
         
         self.slider = QSlider(Qt.Vertical,self)
         self.slider.setMinimum(0)
@@ -128,74 +67,69 @@ class ToDo(QWidget):
         init()
         self.music_load = ""
 
-        self.lineEdit = QLineEdit()
+        central_widget = QWidget()
+        central_widget.setLayout(self.mainGrid)
+        self.setCentralWidget(central_widget)
 
-        self.topPButton = QPushButton('取消置顶')
-        self.topPButton.clicked.connect(self.top)
+        self.menuBar().clear()
+        fileMenu = QMenu('文件', self)
+        self.menuBar().addMenu(fileMenu)
+        musicMenu = QMenu('音乐', self)
+        self.menuBar().addMenu(musicMenu)
+        helpMenu = QMenu('帮助', self)
+        self.menuBar().addMenu(helpMenu)
+        open_file = QAction('打开', self)
+        save_file = QAction('保存', self)
+        upload_m = QAction('导入音乐', self)
+        showhelp = QAction('帮助', self)
+        open_file.triggered.connect(self.open_everywhere)
+        save_file.triggered.connect(self.save_everywhere)
+        upload_m.triggered.connect(self.openmusic)
+        showhelp.triggered.connect(self.show_help)
+        fileMenu.addAction(open_file)
+        fileMenu.addAction(save_file)
+        musicMenu.addAction(upload_m)
+        helpMenu.addAction(showhelp)
 
+        self.line = QFrame()
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setStyleSheet("color: #d6d6d6")
         self.build()
         self.open_()
         self.create_menu()
     #define variables
 
     def build(self):
-        self.mainGrid.addLayout(self.tGrid, 0, 0)
-        self.mainGrid.addLayout(self.bGrid, 0, 1)
-        self.mainGrid.addLayout(self.mGrid, 0, 2)
+        self.mainGrid.addLayout(self.tGrid, 1, 0)
+        self.mainGrid.addLayout(self.bGrid, 1, 1)
         self.tGrid.addWidget(self.label, 0, 0)
-        self.tGrid.addWidget(self.lineEdit, 1, 0)
         self.tGrid.addWidget(self.listWindow, 2, 0)
         self.bGrid.addWidget(self.addPButton, 0, 0)
         self.bGrid.addWidget(self.deletePButton, 1, 0)
-        self.bGrid.addWidget(self.highPButton, 2, 0)
-        self.bGrid.addWidget(self.midPButton, 3, 0)
-        self.bGrid.addWidget(self.lowPButton, 4, 0)
-        self.bGrid.addWidget(self.savePButton, 5, 0)
-        self.bGrid.addWidget(self.openPButton, 6, 0)
-        self.bGrid.addWidget(self.status_label, 7, 0)
-        self.bGrid.addWidget(self.help, 8, 0)
-        self.bGrid.addWidget(self.topPButton,9,0)
-        self.mGrid.addWidget(self.slider,0,0)
-        self.mGrid.addWidget(self.openmPButton,1,0)
-        self.mGrid.addWidget(self.startmPButton,2,0)
-        self.mGrid.addWidget(self.pausemPButton,3,0)
-        self.mGrid.addWidget(self.stopmPButton,4,0)
+        self.bGrid.addWidget(self.line, 2, 0)
+        self.bGrid.addWidget(self.slider,4,0)
+        self.bGrid.addWidget(self.startmPButton,5,0)
+        self.bGrid.addWidget(self.pausemPButton,6,0)
+        self.bGrid.addWidget(self.stopmPButton,7,0)
     #build variable
 
     def add_item(self):
-        if self.lineEdit.text():
-            if not self.status:
-                self.status = 'mid'
-            if self.status == 'high':
-                exp = '高等级'
-            elif self.status == 'mid':
-                exp = '中等级'
-            elif self.status == 'low':
-                exp = '低等级'
-            else:
-                QMessageBox.information(
-                    None,
-                    "操作失败",
-                    "状态有误，已设置为默认等级（中等级）"
-                )
-                exp = '中等级'
+        a = startDialog()
+        if a.exec() == QDialog.Accepted:
+            b = a.save()
             item = QTreeWidgetItem(self.listWindow)
+            if b[1] == '高等级':
+                self.status = 'high'
+            elif b[1] == '中等级':
+                self.status = 'mid'
+            elif b[1] == '低等级':
+                self.status = 'low'
             item.setBackground(0, QColor(self.PRIORITY_INFO[self.status]))
             item.setBackground(1, QColor(self.PRIORITY_INFO[self.status]))
-            item.setText(0, self.lineEdit.text())
-            item.setText(1, exp)
+            item.setText(0, b[0])
+            item.setText(1, b[1])
             self.listWindow.scrollToItem(item)
-            self.lineEdit.clear()
             self.sorted_()
-        else:
-            QMessageBox.information(
-            None,
-            "操作失败",
-            "项名不能为空"
-        )
-            self.lineEdit.clear()
-        self.status = None
-        self.status_label.setText('状态：中等级')
     #add items
 
     def remove_item(self):
@@ -216,19 +150,6 @@ class ToDo(QWidget):
                 "未选中项目"
             )
     #remove items
-
-    def high(self):
-        self.status = 'high'
-        self.status_label.setText('状态：高等级')
-
-    def mid(self):
-        self.status = 'mid'
-        self.status_label.setText('状态：中等级')
-
-    def low(self):
-        self.status = 'low'
-        self.status_label.setText('状态：低等级')
-    #change level
 
     def save_model(self, filename):
         tasks = {}
@@ -298,6 +219,8 @@ class ToDo(QWidget):
             item = self.listWindow.topLevelItem(i)
             name = item.text(0)
             level = item.text(1)
+            if level not in priority_order:
+                level = "中等级"
             tasks.append({'name': name, 'level': level})
         tasks.sort(key=lambda x: priority_order[x['level']], reverse=True)
         self.listWindow.clear()
@@ -410,34 +333,6 @@ class ToDo(QWidget):
         self.save()
     #save backup
 
-    def top(self):
-        handle = self.windowHandle()
-        
-        flags = handle.flags()
-        
-        if self.top_value:
-            flags &= ~Qt.WindowStaysOnTopHint
-            flags |= Qt.WindowCloseButtonHint
-            flags |= Qt.WindowMinimizeButtonHint
-            flags |= Qt.WindowMaximizeButtonHint
-            handle.setFlags(flags)
-            self.top_value = False
-            self.topPButton.setText('置顶')
-        else:
-            flags |= Qt.WindowStaysOnTopHint
-            flags |= Qt.WindowCloseButtonHint
-            flags |= Qt.WindowMinimizeButtonHint
-            flags |= Qt.WindowMaximizeButtonHint
-            handle.setFlags(flags)
-            self.top_value = True
-            self.topPButton.setText('取消置顶')
-        
-        new_flags = handle.flags()
-        
-        self.raise_()
-        self.activateWindow()
-        
-
 class editDialog(QDialog):
     def __init__(self, item_name, item_level):
         super().__init__()
@@ -492,3 +387,58 @@ class editDialog(QDialog):
         b = [a, level]
         return b
     #save
+
+class startDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(f'创建任务')
+        self.mainLayout = QGridLayout()
+        self.setLayout(self.mainLayout)
+        self.f_gird = QGridLayout()
+        self.s_gird = QGridLayout()
+        self.t_gird = QGridLayout()
+        self.text = QLineEdit()
+        self.label = QLabel('重命名')
+        self.radio1 = QRadioButton("高等级")
+        self.radio1.clicked.connect(lambda: self.choose("高等级"))
+        self.radio2 = QRadioButton("中等级")
+        self.radio2.clicked.connect(lambda: self.choose("中等级"))
+        self.radio3 = QRadioButton("低等级")
+        self.radio3.clicked.connect(lambda: self.choose("低等级"))
+        self.turePButton = QPushButton('确定')
+        self.turePButton.clicked.connect(self.accept)
+        self.falsePButton = QPushButton('取消')
+        self.falsePButton.clicked.connect(self.reject)
+        self.level = "中等级"
+        if self.level == "高等级":
+            self.radio1.setChecked(True)
+        elif self.level == "中等级":
+            self.radio2.setChecked(True)
+        else:
+            self.radio3.setChecked(True)
+        self.build()
+        # define variable
+
+    def build(self):
+        self.mainLayout.addLayout(self.f_gird, 0, 0)
+        self.mainLayout.addLayout(self.s_gird, 1, 0)
+        self.mainLayout.addLayout(self.t_gird, 2, 0)
+        self.f_gird.addWidget(self.label, 0, 0)
+        self.f_gird.addWidget(self.text, 1, 0)
+        self.s_gird.addWidget(self.radio1, 0, 0)
+        self.s_gird.addWidget(self.radio2, 0, 1)
+        self.s_gird.addWidget(self.radio3, 0, 2)
+        self.t_gird.addWidget(self.turePButton, 0, 0)
+        self.t_gird.addWidget(self.falsePButton, 0, 1)
+        # build variable
+
+    def choose(self, name):
+        self.level = name
+        # choose level
+
+    def save(self):
+        a = self.text.text()
+        level = self.level
+        b = [a, level]
+        return b
+    # save
